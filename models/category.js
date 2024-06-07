9df8e75204565d7301e70accb3f90c810a0d8112
Antonio Zamora <antoniozamora21331@gmail.com>
2024-06-06 17:26:38 -0700

    Setup the database schemas and models

---
+++ models/category.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const categorySchema = new Schema({
+  name: { type: String, required: true, maxlength: 100 },
+  description: { type: String, required: true },
+});
+
+categorySchema.virtual("url").get(function () {
+  return `/shop/category/${this._id}`;
+});
+
+module.exports = mongoose.model("Category", categorySchema);
