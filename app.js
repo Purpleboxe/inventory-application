d607143ef8091b88536a31b3563ec29bfbacb92d
Antonio Zamora <antoniozamora21331@gmail.com>
2024-06-09 15:27:09 -0700

    Add basic setup for controllers and routers

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
+const shopRouter = require("./routes/shop");
 
-var app = express();
+const app = express();
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB =
-  "mongodb+srv://myAtlasDBUser:Lamalama1!@cluster0.wr1nyrw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-main().catch((err) => console.log(err));
+const mongoDB =
+  "mongodb+srv://myAtlasDBUser:Thochiwilen5!@cluster0.wr1nyrw.mongodb.net/inventory_application?retryWrites=true&w=majority&appName=Cluster0";
+
 async function main() {
-  await mongoose.connect(mongoDB);
+  try {
+    await mongoose.connect(mongoDB);
+    console.log("Connected to MongoDB");
+  } catch (err) {
+    console.error("MongoDB connection error:", err);
+  }
 }
+
+main();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -30,14 +38,15 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/shop", shopRouter);
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use((req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+
+const item_controller = require("../controllers/itemController");
+const category_controller = require("../controllers/categoryController");
+
+router.get("/", item_controller.index);
+
+// Category Routes
+
+router.get("/categories", category_controller.category_list);
+
+module.exports = router;
