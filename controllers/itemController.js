58f4ef5a5f815781f75da7b320ced8df3484e87d
Antonio Zamora <antoniozamora21331@gmail.com>
2024-06-10 16:19:16 -0700

    Add some new styles and make a homepage

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,6 +1,17 @@
-const item = require("../models/item");
+const Item = require("../models/item");
+const Category = require("../models/category");
+
 const asyncHandler = require("express-async-handler");
 
 exports.index = asyncHandler(async (req, res, next) => {
-  res.send("Homepage");
+  const [numItems, numCategories] = await Promise.all([
+    Item.countDocuments({}).exec(),
+    Category.countDocuments({}).exec(),
+  ]);
+
+  res.render("index", {
+    title: "Home",
+    item_count: numItems,
+    category_count: numCategories,
+  });
 });
