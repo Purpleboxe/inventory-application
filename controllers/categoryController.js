fd8b917a355956598f8d8a69409af6e0b01badef
Antonio Zamora <antoniozamora21331@gmail.com>
2024-06-11 16:24:24 -0700

    Update views, populatedb.js, and add a view for category details

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,5 @@
 const Category = require("../models/category");
+const Item = require("../models/item");
 const asyncHandler = require("express-async-handler");
 
 exports.category_list = asyncHandler(async (req, res, next) => {
@@ -7,5 +8,24 @@ exports.category_list = asyncHandler(async (req, res, next) => {
   res.render("category_list", {
     title: "Category List",
     category_list: allCategories,
+  });
+});
+
+exports.category_detail = asyncHandler(async (req, res, next) => {
+  const [category, itemsInCategory] = await Promise.all([
+    Category.findById(req.params.id).exec(),
+    Item.find({ category: req.params.id }, "name description").exec(),
+  ]);
+
+  if (category === null) {
+    const err = new Error("Category not found!");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("category_detail", {
+    title: "Category Detail",
+    category: category,
+    category_items: itemsInCategory,
   });
 });
