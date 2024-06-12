2537586a8994b4ce3128a77b2d409e721746f387
Antonio Zamora <antoniozamora21331@gmail.com>
2024-06-12 14:02:33 -0700

    Update the item detail view

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -24,3 +24,18 @@ exports.item_list = asyncHandler(async (req, res, next) => {
 
   res.render("item_list", { title: "Item List", item_list: allItems });
 });
+
+exports.item_detail = asyncHandler(async (req, res, next) => {
+  const item = await Item.findById(req.params.id).populate("category").exec();
+
+  if (item === null) {
+    const err = new Error("Item not found!");
+    err.status = 404;
+    return next(err);
+  }
+
+  res.render("item_detail", {
+    title: "Item: " + item.name,
+    item: item,
+  });
+});
